dc782b94fe1dabab6212d6919e174ce7c2935f47
m3k0813 <m3k0813@gmail.com>
2021-01-19 22:51:18 +0900

    Memo - Dialog Updata and Delete

--- app/src/main/java/com/example/notebook3/RecyclerAdapter.java
+++ app/src/main/java/com/example/notebook3/RecyclerAdapter.java
@@ -1,11 +1,20 @@
 package com.example.notebook3;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -14,15 +23,19 @@ import androidx.room.Database;
 import com.google.android.material.tabs.TabItem;
 
 import java.util.ArrayList;
+import java.util.Dictionary;
 
 public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ItemViewHolder> {
 
     private ArrayList<Memo> listData = new ArrayList<>();
+    private Context context;
+    private MemoDatabase db;
 
     @NonNull
     @Override
     public ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.note_item, parent, false);
+        context = parent.getContext();     //context 생성
         return new ItemViewHolder(view);
     }
 
@@ -65,15 +78,33 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ItemVi
             textView1.setText(memo.getTitle());
             textView2.setText(memo.getContent());
 
-            //길게 클릭 시 수정화면
+            //길게 클릭 시 수정, 삭제 다이얼로그
             itemView.setOnLongClickListener(v -> {
-                Intent intent = new Intent(itemView.getContext(), MemoDetailActivity.class);
-                intent.putExtra("data", memo);
-                itemView.getContext().startActivity(intent);
+            AlertDialog.Builder ab = new AlertDialog.Builder(context); //context 객체를 생성해야 함
+                ab.setTitle("메모").setMessage("메모 선택");
+                ab.setNeutralButton("수정", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(itemView.getContext(), MemoDetailActivity.class);
+                        intent.putExtra("data", memo);
+                        itemView.getContext().startActivity(intent);
+                    }
+                });
+
+                ab.setPositiveButton("삭제", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        listData.remove(memo);
+                        MemoDatabase.getInstance(itemView.getContext()).memoDao().delete(memo);
+
+                        notifyDataSetChanged();
+                    }
+                });
+                ab.show();
                 return false;
             });
 
         }
     }
-
 }
+
